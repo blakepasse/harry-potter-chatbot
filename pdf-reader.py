f8488a45578d11c2a317e8071c11399166531a1d
Blake Passe <blakepasse@gmail.com>
2024-07-21 15:09:38 -0500

    removed text splitters due to package conflict. imported other packages to toml

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,30 +1,27 @@
 import os
 from pypdf import PdfReader
 
+# Extract text from pdfs
 pdf_path = "/Users/bpasse/Desktop/virtual-tests/project/documents/sample-newspaper.pdf"
 
-# Define the directory and file name for the output text file
 output_dir = "/Users/bpasse/Desktop/virtual-tests/project/converted"
 output_file = "extracted_text.txt"
 
-# Define the full path to the output text file
 output_path = os.path.join(output_dir, output_file)
 
-# Read the PDF and extract text
 reader = PdfReader(pdf_path)
 extracted_text = ""
 for page in reader.pages:
     extracted_text += page.extract_text()
 
-# Write the extracted text to the output file
 with open(output_path, "w") as text_file:
     text_file.write(extracted_text)
 
 
-# This is a long document we can split up.
 with open("/Users/bpasse/Desktop/virtual-tests/project/converted/extracted_text.txt") as f:
     text = f.read()
 
+# Chunk text
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 text_splitter = RecursiveCharacterTextSplitter(
@@ -36,3 +33,6 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 texts = text_splitter.create_documents([text])
+
+# Embed chunks in pinecone
+# Use chatbot
